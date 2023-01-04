1feef63a8b74eb8e01d48101b52c871361141cfe
Sujan804 <sujanmec804@gmail.com>
2023-01-04 22:02:44 +0600

    [BACK] Authentication removed

--- client/src/components/Admin/AdminHome.jsx
+++ client/src/components/Admin/AdminHome.jsx
@@ -1,47 +1,46 @@
-import axios from "axios";
-import React, { useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import profilePic from "../../assets/images/profile.jpg";
 import AdminSidebar from "./Sidebar/AdminSidebar";
 const AdminHome = () => {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
-  try {
-    useEffect(
-      () => async () => {
-        axios
-          .get("http://localhost:5000/admin/", {
-            headers: {
-              Authorization: token,
-            },
-          })
-          .then((res) => {
-            console.log(res);
-            if (res.status === 200) navigate("/admin");
-          })
-          .catch((err) => {
-            console.log(err.message);
-            const msg = err.message.split(" ");
-            if (msg.includes("401")) {
-              navigate("/admin/login");
-            }
-            console.log(msg);
-            // navigate("/admin/login");
-          });
-        // if (res) {
-        //   console.log("::;");
-        //   console.log(res.data);
-        //   navigate("/admin");
-        // } else {
-        //   console.log(res.data);
-        //   navigate("admin/login");
-        // }
-      },
-      []
-    );
-  } catch (error) {
-    // console.log(error);
-  }
+  // try {
+  //   useEffect(
+  //     () => async () => {
+  //       axios
+  //         .get("http://localhost:5000/admin/", {
+  //           headers: {
+  //             Authorization: token,
+  //           },
+  //         })
+  //         .then((res) => {
+  //           console.log(res);
+  //           if (res.status === 200) navigate("/admin");
+  //         })
+  //         .catch((err) => {
+  //           console.log(err.message);
+  //           const msg = err.message.split(" ");
+  //           if (msg.includes("401")) {
+  //             navigate("/admin/login");
+  //           }
+  //           console.log(msg);
+  //           // navigate("/admin/login");
+  //         });
+  //       // if (res) {
+  //       //   console.log("::;");
+  //       //   console.log(res.data);
+  //       //   navigate("/admin");
+  //       // } else {
+  //       //   console.log(res.data);
+  //       //   navigate("admin/login");
+  //       // }
+  //     },
+  //     []
+  //   );
+  // } catch (error) {
+  //   // console.log(error);
+  // }
 
   return (
     <section className="grid grid-cols-12  min-h-screen">
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -59,6 +59,14 @@ export default function Login() {
                     Login
                   </button>
                 </Link>
+                <Link to="/admin" className="ml-4">
+                  <button
+                    type="button"
+                    className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                  >
+                    Login as Admin
+                  </button>
+                </Link>
 
                 <p className="text-sm font-semibold mt-2 pt-1 mb-0">
                   Don't have an account?
